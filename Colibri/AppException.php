9fc0932e3447143f9cadaf1d25d52ad41022ab41
spawnnnn <vahan.grigoryan@gmail.com>
2020-05-13 19:55:21 +0300

    Extends AppException Derived error return from WebServer

--- Colibri/AppException.php
+++ Colibri/AppException.php
@@ -2,7 +2,34 @@
 
     namespace Colibri {
 
+        use Throwable;
+
         class AppException extends \Exception {
+
+            private $_appData;
+
+            /**
+             * Constructor
+             *
+             * @param string $message
+             * @param int $code
+             * @param mixed $appData
+             * @param Throwable $previous
+             */
+            public function __construct($message, $code, $appData, Throwable $previous = null)
+            {
+                parent::__construct($message, $code, $previous);
+                $this->_appData = $appData;
+            }
+
+            /**
+             * Returns custom app data 
+             *
+             * @return mixed
+             */
+            public function getAppData() {
+                return $this->_appData;
+            }
 
         }
 
