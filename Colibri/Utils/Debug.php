0e3d61197f50b3bf1c6f64389acac078bb1beb45
spawn <vahan.grigoryan@gmail.com>
2020-09-10 09:14:58 +0300

    Inprooved Debug IOut method

--- Colibri/Utils/Debug.php
+++ Colibri/Utils/Debug.php
@@ -79,15 +79,15 @@
                     App::Logger()->WriteLine(Logger::Error, func_get_args());
                 }
                 else if($mode === App::ModeDevelopment) {
-                    $clickevent = "onclick='javascript: iout_toggle(event);'";
+                    $clickevent = 'onclick="event.currentTarget.parentElement.nextElementSibling && (event.currentTarget.parentElement.nextElementSibling.style.display = event.currentTarget.parentElement.nextElementSibling.style.display == \'\' ? \'none\' : \'\');"';
                     $result = self::_createArgs(func_get_args());
                     $result = print_r($result, true);
                     $result = str_replace("<", "&lt;", str_replace(">", "&gt;", $result));
                     $result = preg_replace("/\s*?\[(.*)\] \=&gt; (.*?)\n/mi", "\n<div class='legend' ".$clickevent.">[\$1] => \$2</div>\n", $result);
                     $result = preg_replace("/(<div class='legend' ".preg_quote($clickevent).">.*<\/div>)\n\s*?\(/mi", "\n<div class='object'><div class='hilite'>\$1</div><div class='children' style='display: none'>\n", $result);
                     $result = preg_replace("/\n\s*?\)\n/", "\n</div></div>\n", $result);
-                    $result = preg_replace("/Array\n\(\n/i", "\n<div class='result'><div class='object'><div class='legend' ".$clickevent.">IOUT - Result</div><div class='children'>\n", $result);
-                    echo '<style type="text/css"> div.legend { cursor: default; cursor: expression("hand"); padding-top: 2px; padding-bottom: 2px; } div.legend span { margin-left: 5px; } div.object { font-size: 12px; font-family: courier new; } div.children { margin-left: 50px; padding-top: 1px; padding-bottom: 1px; border-left: 1px solid #f9f9f9; min-height: 5px; height: expression("5px"); } div.result { border: 1px solid #f2f2f2; padding: 10px;} div.hilite { color: #050; }</style><script language="javascript">function iout_toggle(e) { let p = e.srcElement ? e.srcElement.parentElement : e.currentTarget.parentNode; if(p.className == "hilite") { p = e.srcElement ? parent.parentElement : parent.parentNode; let c = p.childNodes[1]; c.style.display = c.style.display == "" ? "none" : ""; } }</script>'.$result.'</div>';
+                    $result = preg_replace("/Array\n\(\n/i", "\n<div class='result'><div class='object'><div class='legend' ".$clickevent.">IOUT - Result</div><div class='children'>\n", $result).'</div>';
+                    echo $result.'<style type="text/css">div.result { border: 1px solid #f2f2f2; padding: 10px;} div.legend { cursor: pointer; padding: 2px 0; } div.object { font: 12px monospace; } div.children { padding: 1px 0 1px 50px; border-left: 1px solid #f9f9f9; min-height: 5px; } div.hilite { color: #050; }</style>';
                 }
             }
 
